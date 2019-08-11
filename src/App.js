023e95fac5bf34ef7f9bc9bcee49fffdf17bc01b
Md Abu Zafar <mdabuzafar78@gmail.com>
2019-08-11 12:19:17 +0530

    [Feat] Add Conditional rendering for clear button

--- src/App.js
+++ src/App.js
@@ -39,12 +39,23 @@ class App extends Component {
     });
   };
 
+  clearUsers = () => {
+    this.setState({
+      users: [],
+      loading: false,
+    });
+  };
+
   render() {
     return (
       <div className='App'>
         <Navbar />
         <div className='container'>
-          <Search searchUsers={this.searchUsers} />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={this.state.users.length > 0}
+          />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
