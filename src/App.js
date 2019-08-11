6352552b867956958d1dd15413d18bcfe58aa94f
Md Abu Zafar <mdabuzafar78@gmail.com>
2019-08-11 11:23:17 +0530

    Add axios and fetch data from github

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
 import './App.css';
 
 class App extends Component {
+  state = {
+    loading: false,
+    users: [],
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get('https://api.github.com/users');
+    this.setState({
+      users: res.data,
+      loading: false,
+    });
+  }
+
   render() {
     return (
       <div className='App'>
-        <h1>Hello from react</h1>
+        <Navbar />
+        <div className='container'>
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>
       </div>
     );
   }
