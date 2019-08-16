979cb6e91ce0a2acd1eb8dac077713739dc8fcb1
Md Abu Zafar <mdabuzafar78@gmail.com>
2019-08-16 14:25:45 +0530

    [Feat] Add repos in the User page

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App extends Component {
     loading: false,
     users: [],
     user: {},
+    repos: [],
     alert: null,
   };
 
@@ -61,6 +62,22 @@ class App extends Component {
     });
   };
 
+  // get users repos
+  getUserRepos = async (username) => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`,
+    );
+
+    this.setState({
+      repos: res.data,
+      loading: false,
+    });
+  };
+
   clearUsers = () => {
     this.setState({
       users: [],
@@ -84,7 +101,7 @@ class App extends Component {
   };
 
   render() {
-    const { loading, user, users } = this.state;
+    const { loading, user, users, repos } = this.state;
     return (
       <Router>
         <div className='App'>
@@ -115,7 +132,9 @@ class App extends Component {
                   <User
                     {...props}
                     getUser={this.getUser}
+                    getUserRepos={this.getUserRepos}
                     user={user}
+                    repos={repos}
                     loading={loading}
                   />
                 )}
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,5 +1,6 @@
 import React, { Fragment, Component } from 'react';
 import Spinner from '../layout/Spinner';
+import Repos from '../repos/Repos';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
@@ -7,12 +8,15 @@ export class User extends Component {
   componentDidMount() {
     const username = this.props.match.params.login;
     this.props.getUser(username);
+    this.props.getUserRepos(username);
   }
 
   static propTypes = {
     loading: PropTypes.bool.isRequired,
     user: PropTypes.object.isRequired,
+    repo: PropTypes.array.isRequired,
     getUser: PropTypes.func.isRequired,
+    getUserRepos: PropTypes.func.isRequired,
   };
 
   render() {
@@ -101,6 +105,7 @@ export class User extends Component {
           <div className='badge badge-light'>Public Repos: {public_repos}</div>
           <div className='badge badge-dark'>Public Gists: {public_gists}</div>
         </div>
+        <Repos repos={this.props.repos} />
       </Fragment>
     );
   }
